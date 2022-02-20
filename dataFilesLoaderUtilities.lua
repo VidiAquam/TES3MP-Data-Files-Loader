2d671ed630cd940ce55558832764486bf3d4ce28
Vidi_Aquam <89811652+VidiAquam@users.noreply.github.com>
2022-02-19 19:15:57 -0600

    Improvements to updateCellRefs()

--- dataFilesLoaderUtilities.lua
+++ dataFilesLoaderUtilities.lua
@@ -8,13 +8,21 @@ dataFilesLoader.updateCellRefs = function(cellDescription, oldCell, newCell)
                 if oldRefnum:endswith("-0") and oldCell.references[oldRefnum:trimend("-0")] ~= nil then -- Is it an object in an esm/esp?
 
                     local oldRefData = oldCell.references[oldRefnum:trimend("-0")] -- Get ref data from the old cell entry
-
+                    local newRefnum
                     for refnum, ref in ipairs(newCell.references) do
                         if ref.id == oldRefData.id and ref.translation == oldRefData.translation and ref.rotation == oldRefData.rotation then -- Check if object is same w/ different refnum
-                            local newRefnum = refnum
+                            newRefnum = refnum .. "-0"
 
                             serverCellInfo.objectData[newRefnum] = serverCellInfo.objectData[oldRefnum] -- Update object data to new refnum
                             serverCellInfo.objectData[oldRefnum] = nil
+                            
+                            for _, packets in pairs(serverCellInfo.packets) do
+                                if tableHelper.containsValue(packets, oldRefnum, true) then
+                                    table.remove(packets,tableHelper.getIndexByValue(packets, oldRefnum))
+                                    table.insert(packets, newRefnum)
+                                end
+                            end
+                            
                             break
                         end
                     end
