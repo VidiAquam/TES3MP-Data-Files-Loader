df38e2a3321f3e13f2dfe9656b8d61ebe1b8d187
Ollie Rees <ollie@Telredor.lan>
2022-06-09 15:12:47 +0100

    Case insensitive DFL key fix

--- dataFilesLoaderUtilities.lua
+++ dataFilesLoaderUtilities.lua
@@ -47,10 +47,26 @@ dataFilesLoader.getRecord = function(id, recordType)
         return dataFilesLoader.data[recordType]
     end
     if recordType ~= "Cell" then
-        if dataFilesLoader.data[recordType] ~= nil then return dataFilesLoader.data[recordType][id] else return nil end
+        if dataFilesLoader.data[recordType] ~= nil then
+            if dataFilesLoader.data[recordType][id] ~= nil then 
+                return dataFilesLoader.data[recordType][id]
+            end
+            local newId = dataFilesLoader.getCaseInsensitiveKey(id, recordType) 
+            if newId ~= nil then
+                return dataFilesLoader.data[recordType][newId]
+            end
+        end
+        return nil
     else
         return dataFilesLoader.getCellRecord(id)
     end
+end
+
+dataFilesLoader.getCaseInsensitiveKey = function(id, recordType) 
+    for key, _ in pairs(dataFilesLoader.data[recordType]) do
+        if key:lower() == id:lower() then return key end
+    end
+    return nil
 end
 
 dataFilesLoader.getCellRecord = function(cellDescription)
